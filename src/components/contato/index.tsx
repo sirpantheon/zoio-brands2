ee0bf2d539e96963546f1f7b40d259fc6e19d0ee
johnatan sousa paixao <johnatan.sousa@gmail.com>
2023-08-20 20:57:06 -0300

    contato

---
+++ src/components/contato/index.tsx
@@ -0,0 +1,22 @@
+import styles from './contato.module.sass'
+
+export default function Contato() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+
+        <h2>Entre em contato conosco</h2>
+
+        <form>
+          <label>Nome</label>
+          <input placeholder='Seu nome'></input>
+          <label>Seu email*</label>
+          <input placeholder='Seu endereço de email'></input>
+          <label>Mensagem*</label>
+          <input placeholder='Digite sua Mensagem'></input>
+          <button type='submit'>Enviar</button>
+        </form>
+      </div>
+    </section>
+  )
+}
