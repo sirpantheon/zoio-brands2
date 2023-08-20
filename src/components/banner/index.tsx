b2afb4d9459799d1b527aaa2eb853f6171f1e9bd
johnatan sousa paixao <johnatan.sousa@gmail.com>
2023-08-20 20:56:57 -0300

    banner

---
+++ src/components/banner/index.tsx
@@ -0,0 +1,13 @@
+import styles from './banner.module.sass'
+
+export default function Banner() {
+  return (
+    <footer className={styles.banner}>
+      <div>
+
+        <span className={styles.span1}>Veja sua marca</span> <span className={styles.span2}>com outros olhos</span>
+
+      </div>
+    </footer>
+  )
+}
