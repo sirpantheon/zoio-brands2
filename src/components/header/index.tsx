4400a495f7723be6b60a7d860629a49c8467a0d3
johnatan sousa paixao <johnatan.sousa@gmail.com>
2023-08-20 20:57:27 -0300

    header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import LogoGif from '../../../public/logo.gif'
+import styles from './header.module.sass'
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header className={styles.container}>
+      <div className={styles.header}>
+
+        <div className={styles.logo}>
+          <Image src={LogoGif} height={308} width={308} alt={"logo"} />
+        </div>
+
+        <div className={styles.menu}>
+          <button><Link href={"/"}>Inicio</Link></button>
+          <button><Link href={"/sobre"}>Sobre</Link></button>
+          <button><Link href={"/projetos"}>Projetos</Link></button>
+          <button><Link href={"/contato"}>Contato</Link></button>
+        </div>
+
+      </div>
+    </header>
+  )
+}
