d4e2fe6a74694e775b7ee5e3944040b02c909cd6
johnatan sousa paixao <johnatan.sousa@gmail.com>
2023-09-19 21:18:50 -0300

    projetos

--- src/components/projetosSelect/index.tsx
+++ src/components/projetosSelect/index.tsx
@@ -1,13 +1,18 @@
-import CarouselComponent from '../carousel/CarouselComponent'
 import styles from './projetoSelect.module.sass'
+import Carousel from '../carousel';
+
 
 export default function ProjetoSelect() {
+
   return (
     <section className={styles.container}>
       <div className={styles.content}>
         <span className={styles.title}>Projeto Selecionados</span>
         <div className={styles.projeto}>
-          <CarouselComponent />
+          <div className={styles.ContainerCarousel}>
+            <Carousel />
+ 
+          </div>
         </div>
       </div>
     </section>
